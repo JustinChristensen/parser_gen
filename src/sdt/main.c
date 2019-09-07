e29be9ce8a0f11b02ca56a1cda0cc079246b1f28
Justin Christensen <wroathe@gmail.com>
2019-09-07 16:20:41 -0500

    added valgrind check to sdt makefile

--- src/sdt/main.c
+++ src/sdt/main.c
@@ -27,7 +27,7 @@ enum opt_values {
     DEBUG
 };
 
-struct option_descriptor opt_descs[] = {
+static struct option_descriptor opt_descs[] = {
     { { "help",      no_argument,  NULL,  'h' },       "Print help" },
     { { "version",   no_argument,  NULL,  VERSION },   "Print version information" },
     { { "scan-only", no_argument,  NULL,  SCAN_ONLY }, "Print the result of scanning the input stream" },
@@ -35,22 +35,23 @@ struct option_descriptor opt_descs[] = {
     { { NULL,         0,           NULL,  0 },          NULL }
 };
 
-struct option *options(struct option_descriptor *opt_descs) {
-    static struct option opts[sizeof opt_descs / sizeof *opt_descs];
+static struct option opts[sizeof(opt_descs) / sizeof((opt_descs)[0])];
 
+struct option *options(struct option_descriptor *opt_descs) {
     for (int i = 0; i < sizeof opt_descs; i++) {
         opts[i] = opt_descs[i].option;
     }
+
     return opts;
 }
 
 #define FLAG_INDENT " "
 #define DESC_SEP "    "
 
-void print_usage(char *prog_name, FILE *handle) {
+void print_usage(char *prog_name, FILE *handle, size_t num_descs) {
     fprintf(handle, "usage: %-s [options]\n\n", prog_name);
     fprintf(handle, "options:\n");
-    for (int i = 0; i < sizeof opt_descs / sizeof *opt_descs; i++) {
+    for (int i = 0; i < num_descs; i++) {
         char *desc  = opt_descs[i].description;
         if (desc) {
             struct option opt = opt_descs[i].option;
@@ -71,12 +72,13 @@ struct args read_args(int argc, char *argv[]) {
         .scan_only = false,
         .debug_parser = false
     };
+    size_t num_descs = sizeof(opt_descs) / sizeof((opt_descs)[0]);
     int f;
 
-    while ((f = getopt_long(argc, argv, "h", options(opt_descs), NULL)) != -1) {
+    while ((f = getopt_long(argc, argv, "h", options(opt_descs), NULL) != -1)) {
         switch (f) {
             case 'h':
-                print_usage(argv[0], stdout);
+                print_usage(argv[0], stdout, num_descs);
                 exit(EXIT_SUCCESS);
                 break;
             case VERSION:
@@ -90,7 +92,7 @@ struct args read_args(int argc, char *argv[]) {
                 args.debug_parser = true;
                 break;
             default:
-                print_usage(argv[0], stderr);
+                print_usage(argv[0], stderr, num_descs);
                 exit(EXIT_FAILURE);
         }
     }

bc76cda166ea3c36ad4c7b110b7f14b478984641
Justin Christensen <wroathe@gmail.com>
2020-01-05 17:56:40 -0600

    first draft of the grammar grammar

--- libs/gram/gram/parser.h
+++ libs/gram/gram/parser.h
@@ -5,7 +5,28 @@
 #include <stdio.h>
 
 /*
-    grammar =
+nl            \n
+alpha         [A-Za-z_]
+alnum         {alpha}|[0-9]
+identifier    {alpha}{alnum}*
+literal       ('{alnum}+'|"{alnum}+")
+symbol        ({identifier}|{literal})
+expression    {non-nl}
+
+===
+
+grammar     = header rules eof;
+header      = token_defs nl "===" nl nl | ε;
+token_defs  = token_def token_defs
+token_def   = identifier expression nl
+rules       = rule rules | ε;
+lhs         = identifier;
+rule        = lhs '=' alts ';';
+alts        = rhses alts_tail;
+alts_tail   = '|' rhses alts_tail | ε;
+rhses       = rhs rhses_tail;
+rhses_tail  = rhs rhses_tail | ε;
+rhs         = symbol | "$empty";
 */
 
 #endif // GRAM_PARSER_H_

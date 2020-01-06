5bfab137e8367f94d13469cfd2d2bec766214e3b
Justin Christensen <wroathe@gmail.com>
2020-01-05 18:14:46 -0600

    added ast header to gram

---
+++ libs/gram/gram/ast.h
@@ -0,0 +1,36 @@
+#ifndef GRAM_AST_H_
+#define GRAM_AST_H_ 1
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct grammar_node {
+};
+
+struct header_node {
+};
+
+struct token_defs_node {
+};
+
+struct token_def_node {
+};
+
+struct rules_node {
+};
+
+struct rule_node {
+};
+
+struct alts_node {
+};
+
+struct rhses_node {
+};
+
+struct rhs_node {
+};
+
+#endif // GRAM_AST_H_
+
+
--- libs/gram/gram/parser.h
+++ libs/gram/gram/parser.h
@@ -9,24 +9,54 @@ nl            \n
 alpha         [A-Za-z_]
 alnum         {alpha}|[0-9]
 identifier    {alpha}{alnum}*
-literal       ('{alnum}+'|"{alnum}+")
+char          '{alnum}+'
+string        "{alnum}+"
+literal       ({char}|{string})
 symbol        ({identifier}|{literal})
 expression    {non-nl}
 
 ===
 
-grammar     = header rules eof;
-header      = token_defs nl "===" nl nl | ε;
-token_defs  = token_def token_defs
-token_def   = identifier expression nl
-rules       = rule rules | ε;
-lhs         = identifier;
-rule        = lhs '=' alts ';';
-alts        = rhses alts_tail;
-alts_tail   = '|' rhses alts_tail | ε;
-rhses       = rhs rhses_tail;
-rhses_tail  = rhs rhses_tail | ε;
-rhs         = symbol | "$empty";
+grammar         = header rules eof;
+header          = token_defs nl "===" nl nl | $empty;
+token_defs      = token_def token_defs_tail;
+token_defs_tail = token_def token_defs_tail | $empty;
+token_def       = identifier expression nl;
+rules           = rule rules | $empty;
+lhs             = identifier;
+rule            = lhs '=' alts ';';
+alts            = rhses alts_tail;
+alts_tail       = '|' rhses alts_tail | $empty;
+rhses           = rhs rhses_tail;
+rhses_tail      = rhs rhses_tail | $empty;
+rhs             = symbol | "$empty";
 */
+
+enum gram_symbol {
+};
+
+struct gram_scan_context {
+};
+
+struct gram_parse_error {
+};
+
+struct gram_parse_context {
+    void *ast;
+};
+
+struct gram_scan_context();
+struct gram_parse_context();
+bool parse_grammar(struct gram_parse_context *context);
+bool parse_header(struct gram_parse_context *context);
+bool parse_token_defs(struct gram_parse_context *context);
+bool parse_token_def(struct gram_parse_context *context);
+bool parse_rules(struct gram_parse_context *context);
+bool parse_lhs(struct gram_parse_context *context);
+bool parse_rule(struct gram_parse_context *context);
+bool parse_alts(struct gram_parse_context *context);
+bool parse_rhses(struct gram_parse_context *context);
+bool parse_rhs(struct gram_parse_context *context);
+void write_parser(FILE *handle, struct gram_parse_context *context);
 
 #endif // GRAM_PARSER_H_
